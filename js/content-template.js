fbb8faa707d4df2a43f89bbf9c0d463ce7a91048
eprochnau <proc0035@algonquinlive.com>
2015-01-29 11:28:38 -0500

    feat(all): add initial files

---
+++ js/content-template.js
@@ -0,0 +1,49 @@
+var people = [
+/*
+	{
+		name: 'FirstName LastName',
+		id: 'lastname-firstname',
+		website: 'http://johnsmith.ca',
+		email: 'hey@johnsmith.ca',
+		skills: 'Branding, UX, Web',
+		behance: false,
+		dribbble: false,
+		facebook: 'http://facebook.com/johnsmith'
+		github: false,
+		instagram: 'http://instagram.com/johnsmith',
+		linkedin: false,
+		pinterest: false,
+		twitter: 'http://twitter.com/johnsmith',
+		vimeo: false,
+		works: [
+			'1',
+			'2',
+			'3', **Use the SRC tag from the embed code that Vimeo supplies for any Vimeo submissions**
+			'4'
+		]
+	},
+*/
+
+	{
+		name: 'FirstName LastName',
+		id: 'lastname-firstname',
+		website: 'http://johnsmith.ca',
+		email: 'hey@johnsmith.ca',
+		skills: 'Branding, UX, Web',
+		behance: false,
+		dribbble: false,
+		facebook: 'http://facebook.com/johnsmith'
+		github: false,
+		instagram: 'http://instagram.com/johnsmith',
+		linkedin: false,
+		pinterest: false,
+		twitter: 'http://twitter.com/johnsmith',
+		vimeo: false,
+		works: [
+			'1',
+			'2',
+			'3', **Use the SRC tag from the embed code that Vimeo supplies for any Vimeo submissions**
+			'4'
+		]
+		
+	},
