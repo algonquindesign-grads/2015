c979628120e01a449a5ac134ccb050d6a554aacc
eprochnau <proc0035@algonquinlive.com>
2015-01-29 11:38:52 -0500

    feat(all): add initial files

--- js/content-template.js
+++ js/content-template.js
@@ -25,25 +25,73 @@ var people = [
 */
 
 	{
-		name: 'FirstName LastName',
-		id: 'lastname-firstname',
-		website: 'http://johnsmith.ca',
-		email: 'hey@johnsmith.ca',
-		skills: 'Branding, UX, Web',
-		behance: false,
+		name: 'Colombina Anzivino',
+		id: 'anzivino-colombina',
+		website: 'http://colombinaanzivino.ca',
+		email: 'cmasrs19@gmail.com',
+		skills: 'Branding, Illustration, Print',
+		behance: 'https://www.behance.net/anzivino'
 		dribbble: false,
-		facebook: 'http://facebook.com/johnsmith'
+		facebook: false,
 		github: false,
-		instagram: 'http://instagram.com/johnsmith',
-		linkedin: false,
+		instagram: false,
+		linkedin: 'https://www.linkedin.com/in/colombinaanzivino',
 		pinterest: false,
-		twitter: 'http://twitter.com/johnsmith',
-		vimeo: false,
+		twitter: false,
+		vimeo: 'https://vimeo.com/anzivinocolombina',
 		works: [
 			'1',
 			'2',
-			'3', **Use the SRC tag from the embed code that Vimeo supplies for any Vimeo submissions**
+			'3',
 			'4'
 		]
 		
 	},
+
+	{
+		name: 'Kayla Araujo',
+		id: 'araujo-kayla',
+		website: 'http://kaylaraujo.ca',
+		email: 'hello@kaylaraujo.ca',
+		skills: 'Branding, Print, Typography',
+		behance: 'https://www.behance.net/kaylaaraujo',
+		dribbble: false,
+		facebook: false,
+		github: false,
+		instagram: false,
+		linkedin: 'ca.linkedin.com/pub/kayla-araujo/31/a82/787/',
+		pinterest: false,
+		twitter: 'https://www.twitter.com/kaylafordesign',
+		vimeo: false,
+		works: [
+			'1',
+			'2',
+			'3',
+			'4'
+		]
+
+	}
+
+	{
+		name: 'Jake Asiddao',
+		id: 'asiddao-jake',
+		website: 'http://jakeasiddao.ca/',
+		email: 'jakeasiddao@gmail.com',
+		skills: 'Branding, Print, UI/UX',
+		behance: 'https://www.behance.net/jakeasiddao',
+		dribbble: 'https://www.dribbble.com/jakeasiddao',
+		facebook: false,
+		github: false,
+		instagram: 'http://www.instagram.com/vsiddvo'
+		linkedin: false,
+		pinterest: false,
+		twitter: false,
+		vimeo: false,
+		works: [
+			'1',
+			'2',
+			'3',
+			'4'
+		]
+
+	}
