1c0fb19578facd3a2b31a3ba2db4546aa92d154a
Mark Collantes <collantes.mark.mgc@gmail.com>
2015-02-19 02:36:34 -0500

    feat(html,css&js): add no scroll & smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-$('a[href^="#"]').on('click', function(event) {
-    var target = $( $(this).attr('href') );
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 500);
-    }
+// $('#top').on('click', function(event) {
+//     var target = $( $(this).attr('href') );
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: target.offset().top
+//         }, 500);
+//     }
+// });
+
+var hashTagActive = "";
+$(document).ready( function() {
+	$(".smooth-scroll a").click(function (event) {
+
+		if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+			event.preventDefault();
+
+			//calculate destination place
+			var dest = 0;
+			if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
+				dest = $(document).height() - $(window).height();
+			}
+			else {
+				dest = $(this.hash).offset().top;
+			}
+
+			//go to destination
+			$('html, body').animate({
+			    scrollTop: dest
+			}, 700, 'swing');
+
+			hashTagActive = this.hash;
+		}
+
+	});
 });
+
 
 
 var $about = $('.about');
