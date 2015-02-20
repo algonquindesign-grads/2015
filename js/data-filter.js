b78680f0f313531d5d532dc4949d88d94b2fcb60
Mark Collantes <collantes.mark.mgc@gmail.com>
2015-02-19 21:10:01 -0500

    feat(js): attempt at loading content to overlay
    Please review before merging

---
+++ js/data-filter.js
@@ -0,0 +1,12 @@
+var $studentClick = $('.student a');
+var $lbPic = $('.lightbox-pic');
+var $stuName = $('.student-name');
+var $skillA = $('.skill-a');
+var $skillB = $('.skill-b');
+var $skillC = $('.skill-c');
+var $socA = $('.social-a');
+var $socB = $('.social-b');
+var $socC = $('.social-c');
+var $email = $('.email');
+var $site = $('.site');
+
